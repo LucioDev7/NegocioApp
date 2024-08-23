5260c693eb9751feba290bb750a26c05a7bd901d
LucioDev7 <pianettilucio7@gmail.com>
2024-08-23 14:37:03 -0300

    Agregar archivos de proyecto.

---
+++ NegocioApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NegocioApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ NegocioApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NegocioApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
